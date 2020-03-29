3cee3c2a046b18716e9e4ae5fb0e250796c1b8f9
Bubex <marlon2mrt@gmail.com>
2020-03-29 13:32:31 -0300

    frontend finished

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -34,7 +34,7 @@ export default function Profile() {
                 }
             });
 
-            setIncidents(incidents.filter(incident => incident.id != id));
+            setIncidents(incidents.filter(incident => incident.id !== id));
         } catch (error) {
             alert('Erro ao deletar caso. Tente novamente.');
         }
